18b1015e9b246e7458825b9017b38a1390aa1ce1
misba <misba_2005@.com>
2025-01-09 21:19:17 +0530

    MAke cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,7 +81,16 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
              });
 
         }
-        
-        console.log(cart);
+
+        //Calculate total  Add to cart quantity and put it on page//
+
+
+        let cartQuantity=0;
+        cart.forEach((item)=>{
+            cartQuantity+=item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+       
     });
 });
