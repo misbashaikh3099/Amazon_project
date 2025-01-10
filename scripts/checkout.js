b5f3f85b235508fb7c9391e5a8f5dee736c69970
misba <misba_2005@.com>
2025-01-10 10:46:43 +0530

    Make deliveryOptions Interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart,removeFromCart } from "../data/cart.js";
+import { cart,removeFromCart,updateDeliveryOption} from "../data/cart.js";
 import {products} from '../data/products.js';
 import { formatCurrancy } from "./utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -101,7 +101,9 @@ function deliveryOptionsHTML(matchingProduct,CartItem){
 
     html+=
 
-      `<div class="delivery-option">
+      `<div class="delivery-option js-delivery-option"
+          data-product-id="${matchingProduct.id}"
+           data-delivery-option-id="${deliveryOption.id}">
               <input type="radio"
               ${isChecked? 'checked':''}
                 class="delivery-option-input"
@@ -131,5 +133,13 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
         container.remove();
     });
 
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>
+{
+  element.addEventListener('click',()=>{
+    const{productId,deliveryOptionId}=element.dataset;
+    updateDeliveryOption(productId,deliveryOptionId);
+  });
 });
 
